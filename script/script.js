dbb02363ff9bde3693ee9f46b88367da5c1a7030
LudovicLefieux <lefieux.ludovic@protonmail.com>
2022-08-24 15:17:12 +0200

    Gestion du responsive et correction de bugs

--- script/script.js
+++ script/script.js
@@ -1,37 +1,15 @@
 /*---------- FUNCTIONS -----------*/
 
-// Fait apparaître une modal au clique sur un composant et la ferme si l'on clique sur le cache
-function toggleModal(items) {
-  for (const item of items) {
-    item.addEventListener("click", function () {
-      this.classList.toggle("active");
-
-      const modal = this.nextElementSibling;
-      const cache = document.getElementById("cache");
-
-      modal.classList.toggle("hidden");
-      cache.classList.toggle("hidden");
-
-      cache.addEventListener("click", function (e) {
-        if (e.target == cache) {
-          cache.classList.add("hidden");
-          modal.classList.add("hidden");
-        }
-      });
-    });
-  }
-}
-
 // Fait apparaître un sous-menu lorsqu'on clique sur un élément de menu complexe
 function displayAccordionMenu(items) {
   for (const item of items) {
     item.addEventListener("click", function () {
-      this.classList.toggle("active");
+      this.classList.toggle("js-active");
 
       // Change le "+" en "-" et inversement
       const plus = this.getElementsByClassName("plus")[0];
 
-      if (this.classList.contains("active")) {
+      if (this.classList.contains("js-active")) {
         plus.innerHTML = "-";
       } else {
         plus.innerHTML = "+";
@@ -49,30 +27,111 @@ function displayAccordionMenu(items) {
   }
 }
 
-/*---------- TOGGLE MODALS OF SUBMENU -----------*/
+// Fait apparaître un panel au clique sur un élément du submenu et le ferme si l'on clique ailleurs
+function toggleUserNavPanels(items) {
+  for (const item of items) {
+    const darkOverlay = document.getElementById("darkOverlay");
+    const body = document.getElementsByTagName("body")[0];
 
-const submenuItems = document.querySelectorAll(".submenu > a");
+    item.addEventListener("click", function (event) {
+      let panel = this.nextElementSibling;
+      const activeElements = document.querySelectorAll(
+        ".user-navigation > .js-active, .panel.js-active"
+      );
+      console.log(activeElements);
 
-toggleModal(submenuItems);
+      // Retire la classe js-active de tous les éléments du menu
+      for (const element of activeElements) {
+        element.classList.remove("js-active");
+      }
+
+      // Si l'élément qui suit contient la classe .panel
+      if (panel.classList.contains("panel")) {
+        // Ajoute la classe .js-active au lien et au panel associé
+        this.classList.add("js-active");
+        panel.classList.add("js-active");
+
+        // Bloque le scroll pendant que le panel est ouvert
+        body.style.overflow = "hidden";
+
+        // Gestion de l'interpolation de l'icône du menu burger
+        if (
+          this.classList.contains("burger-menu") ||
+          this.classList.contains("account")
+        ) {
+          darkOverlay.classList.remove("js-active");
+        } else {
+          darkOverlay.classList.toggle("js-active");
+        }
+
+        // Ferme le panel si l'on clique ailleurs
+        window.onclick = function (event) {
+          if (event.target.closest(panel[0])) {
+            return;
+          } else if (item.classList.contains("js-active")) {
+            activeElements.classList.remove("js-active");
+            body.style.overflow = "auto";
+          }
+        };
+      }
+    });
+  }
+}
+
+/*---------- TOGGLE PANELS OF USER NAV -----------*/
+
+const userNavItems = document.querySelectorAll(".user-navigation > a");
+
+toggleUserNavPanels(userNavItems);
 
 /*---------- TOGGLE CLOSE BUTTON OF BURGER MENU -----------*/
 
 let burgerMenu = document.getElementById("burgerMenu");
-const burgerMenuIcon = document.querySelector("#burgerMenu svg:first-child");
-const closeIcon = document.querySelector("#burgerMenu svg:last-child");
+const burgerMenuIcon = document.querySelector(".burger-menu__icon");
+const closeIcon = document.querySelector(".close");
 
 burgerMenu.addEventListener("click", function () {
-  if (burgerMenu.classList.contains("active")) {
-    closeIcon.classList.remove("hidden");
-    burgerMenuIcon.classList.add("hidden");
+  if (burgerMenu.classList.contains("js-active")) {
+    closeIcon.classList.add("js-active");
+    burgerMenuIcon.classList.remove("js-active");
   } else {
-    burgerMenuIcon.classList.remove("hidden");
-    closeIcon.classList.add("hidden");
+    burgerMenuIcon.classList.add("js-active");
+    closeIcon.classList.remove("js-active");
   }
 });
 
 /*---------- TOGGLE SUBMENU OF MOBILE NAV -----------*/
 
-const menuItems = document.querySelectorAll(".mobile-nav__item--complex > a");
+const menuItems = document.querySelectorAll(".main-menu__item--complex > a");
 
 displayAccordionMenu(menuItems);
+
+/*---------- CHANGE SVG COLOR ON HOVER -----------*/
+
+const links = document.querySelectorAll("a");
+
+for (const link of links) {
+  if (link.querySelectorAll("svg") !== null) {
+    const svg = link.getElementsByTagName("path");
+
+    link.addEventListener("mouseover", function () {
+      for (const path of svg) {
+        if (path.hasAttribute("fill")) {
+          path.setAttribute("fill", "#0aac8c");
+        } else {
+          path.setAttribute("stroke", "#0aac8c");
+        }
+      }
+    });
+
+    link.parentNode.addEventListener("mouseout", function () {
+      for (const path of svg) {
+        if (path.hasAttribute("fill")) {
+          path.setAttribute("fill", "#1e1e1e");
+        } else {
+          path.setAttribute("stroke", "#1e1e1e");
+        }
+      }
+    });
+  }
+}
